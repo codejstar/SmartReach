72cf6a9548514f5a37b408d0ac89247c83945b1d
code jstar <codejstar@gmail.com>
2024-11-18 19:18:44 +0530

    create about.html and enpoint of about

--- src/main/java/com/contactmanager/smartcontactmanager/controller/HomePage.java
+++ src/main/java/com/contactmanager/smartcontactmanager/controller/HomePage.java
@@ -1,7 +1,22 @@
 package com.contactmanager.smartcontactmanager.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomePage {
-    public String home() {
+    @GetMapping("/home")
+    public String home(Model model) {
+
+        model.addAttribute("title", "Home - Smart Contact Manager");
         return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+
+        model.addAttribute("title", "About - Smart Contact Manager");
+        return "about";
     }
 }
